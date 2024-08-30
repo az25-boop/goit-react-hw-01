d12eb9b6cea6297c81f2722053fb52986f10e76e
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-08-30 11:25:44 +0300

    css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import FriendListItem from "../FriendListItem/FriendListItem";
-import styles from "./FriendList.module.css";
+import s from "./FriendList.module.css";
 
 const FriendList = ({ friends }) => {
   return (
-    <ul className={styles.friendList}>
-      {friends.map(friend => (
-        <li key={friend.id} className={styles.item}>
-          <FriendListItem 
+    <ul className={s.friendList}>
+      {friends.map((friend) => (
+        <li key={friend.id} className={s.item}>
+          <FriendListItem
             avatar={friend.avatar}
             name={friend.name}
             isOnline={friend.isOnline}
@@ -25,7 +25,7 @@ FriendList.propTypes = {
       avatar: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       isOnline: PropTypes.bool.isRequired,
-      id: PropTypes.number.isRequired
+      id: PropTypes.number.isRequired,
     })
   ).isRequired,
 };
